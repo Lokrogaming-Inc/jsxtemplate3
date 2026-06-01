09465058c947ea63f451e7ad383681722ecadb79
Lokrogaming <lokrogamer.29@gmail.com>
2026-06-02 01:52:52 +0200

    Auto update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -49,7 +49,8 @@ export default function Home() {
        <Divider />
       <section className="my-10 mx-3 flex flex-col items-center">
        <p className="my-7 text-2xl font-bold text-black">Our Services</p>
-      <div className="max-w-[75%] border border-black rounded-xl p-5 bg-black transition-transform duration-300 ease-in-out hover:scale-103 hover:-translate-y-1 "> 
+<div className="grid gris-cols-1 gap-6">
+      <div className="max-w-75 border border-black rounded-xl p-5 bg-black transition-transform duration-300 ease-in-out hover:scale-103 hover:-translate-y-1 "> 
 
         <div className="p-3 my-2">
 <p className="text-yellow-300 border-b border-grey-300 text-2xl font-bold hover:border-white transition-transform duration-300 ease-in-out p-5">Communication & Sales</p>
@@ -75,6 +76,34 @@ export default function Home() {
         
          </ul>
        </div>
+<div className="max-w-75 border border-black rounded-xl p-5 bg-black transition-transform duration-300 ease-in-out hover:scale-103 hover:-translate-y-1 ">
+
+        <div className="p-3 my-2">
+<p className="text-yellow-300 border-b border-grey-300 text-2xl font-bold hover:border-white transition-transform duration-300 ease-in-out p-5">Design of the country pavilion</p>
+          </div>
+       <ul className="list-disc flex flex-col mx-5 my-7 text-white gap-5">
+          <li className="mt-5 mb-3"><p className="text-lg font-bold">Raw Space Design</p></li>
+<li>Conceptualization & Design</li>
+<li>Content Creations</li>
+<li>3D Renderings</li>
+<li>2D Sectional drawings</li>
+<li>Technical Design and Technical Planning</li>
+<li>Urban/ Rural Design</li>
+<li>Service Design</li>
+<li>Human - centered Design</li>
+<li>Furniture Design</li>
+<li>Digital Design</li>
+<li>Content Design and Content Making</li>
+
+<li className="mt-5 mb-3"><p className="text-lg font-bold">Graphic Design</p></li>
+<li>Visual Content Creations</li>
+<li>Design Layout</li>
+<li>Used in catalogues, brochures, marketing flyers, pull-up banners, etc.</li>
+        </ul>
+       </div>
+
+
+</div>
       </section>
     </>
   );
