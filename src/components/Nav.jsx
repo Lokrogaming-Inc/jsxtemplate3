91216534c5b92f784013356a4092cb55a67c48ea
Lokrogamer <lokrogamer.29@gmail.com>
2026-07-22 22:00:42 +0200

    Refactor project structure and update branding. Changed project name from 'eleven-shanghai' to 'jsxtemplate3' in package files. Added Tailwind CSS configuration and new styles for placeholder backgrounds. Updated Hero and NotFound components with new background images and text styles. Added a new image asset and modified Footer and Navbar components for branding consistency.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
-
+  {/*test*/}
   return (
     <>
     {/* Desktop Navbar */}
